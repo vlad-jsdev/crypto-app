6031c69fc96a68dc1c30095d4adea5e778926a63
Vlad Manoilo <48211890+vladosxxx@users.noreply.github.com>
2022-05-07 00:45:50 +0300

    first commit

---
+++ pages/MainPage.jsx
@@ -0,0 +1,56 @@
+import NavBar from "../components/NavBar";
+import Image from "next/image";
+import CryptoImg from "../assets/images/crypto.png";
+import MenuButton from "../components/MenuButton";
+import { walletButton } from "../constants/styles";
+
+const MainPage = () => (
+  <main className="flex flex-grow  my-auto max-w-7xl px-4 sm:my-12 sm:px-6 md:my-16 lg:my-20 lg:px-8 xl:my-28">
+    <div className="sm:text-center lg:text-left">
+      <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+        <span className="block xl:inline">Send Crypto</span> <br />
+        <span className="block text-indigo-600 xl:inline">
+          across the world
+        </span>
+      </h1>
+      <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+        Buy, sell and exchange crypto around the world with small taxes.
+      </p>
+      <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+        <MenuButton
+          name="Connect wallet"
+          key="Connect wallet"
+          href="#"
+          buttonStyles="font-medium text-white py-4 px-6 rounded-full bg-indigo-600 hover:bg-indigo-400"
+        />
+      </div>
+    </div>
+    {/*<svg*/}
+    {/*  className="hidden lg:block absolute right-10  h-full text-white transform translate-x-1/2 z-50"*/}
+    {/*  fill="currentColor"*/}
+    {/*  viewBox="0 0 100 100"*/}
+    {/*  preserveAspectRatio="none"*/}
+    {/*  aria-hidden="true"*/}
+    {/*>*/}
+    {/*  <polygon points="50,0 100,0 50,100 0,100" />*/}
+    {/*</svg>*/}
+    <div className="hidden lg:block flex self-center w-1/2">
+      <Image
+        className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+        src={CryptoImg}
+        alt=""
+      />
+    </div>
+    {/*<svg*/}
+    {/*  className="hidden lg:block absolute right-16  h-full text-white  z-50"*/}
+    {/*  fill="currentColor"*/}
+    {/*  viewBox="0 0 100 100"*/}
+    {/*  preserveAspectRatio="none"*/}
+    {/*  aria-hidden="true"*/}
+    {/*>*/}
+    {/*  <polygon points="0 0, 0 100, 30 0" />*/}
+    {/*</svg>*/}
+  </main>
+);
+
+export default MainPage;
---
+++ pages/index.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import NavBar from "../components/NavBar";
+import MainPage from "./MainPage";
+import Footer from "../components/Footer";
+import { useEffect } from "react";
+
+export default function Home() {
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+  return (
+    <div>
+      {/*<Head />*/}
+      <main className="flex flex-col bg-white dark:bg-gray-700 h-screen">
+        <NavBar />
+        <div className="max-w-7xl mx-auto  h-full">
+          <div className="flex flex-col z-10 pb-8 bg-white dark:bg-gray-700">
+            <MainPage />
+          </div>
+        </div>
+        <Footer />
+      </main>
+    </div>
+  );
+}
