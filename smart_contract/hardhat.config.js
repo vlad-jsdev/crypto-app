5ed344f0c93189c0266707e8093451c426733f06
Vlad Manoilo <48211890+vladosxxx@users.noreply.github.com>
2022-05-13 22:52:39 +0300

    added Mainnet

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,5 +1,4 @@
-// https://eth-ropsten.alchemyapi.io/v2/jrprBkvJJUtvralr2t0Etj5GEEwP5tyd
-
+// https://eth-mainnet.alchemyapi.io/v2/jrprBkvJJUtvralr2t0Etj5GEEwP5tyd
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
@@ -7,6 +6,12 @@ module.exports = {
   networks: {
     ropsten: {
       url: "https://eth-ropsten.alchemyapi.io/v2/jrprBkvJJUtvralr2t0Etj5GEEwP5tyd",
+      accounts: [
+        "33719e85b2b7734278b27a3ba8c72ec897a33e7cdb1069e3276de4dd35eeb4e3",
+      ],
+    },
+    ethereum: {
+      url: "https://eth-mainnet.alchemyapi.io/v2/jrprBkvJJUtvralr2t0Etj5GEEwP5tyd",
       accounts: [
         "33719e85b2b7734278b27a3ba8c72ec897a33e7cdb1069e3276de4dd35eeb4e3",
       ],
