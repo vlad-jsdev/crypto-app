b20edd8abacbf4a2739ff6a30a57b5d1a3e6d323
Vlad Manoilo <48211890+vladosxxx@users.noreply.github.com>
2022-05-14 20:40:57 +0300

    change contract address

--- constants/contracts.js
+++ constants/contracts.js
@@ -1,4 +1,4 @@
 import abi from "../smart_contract/artifacts/contracts/Transaction.sol/Transaction.json";
 
 export const contractABI = abi.abi;
-export const contractAddress = "0xB16a2d135bb007979D98f1bdB3DB23cFa2cC1777";
+export const contractAddress = "0xb96f1a785220D2A6ce392ee67dB6B7704FAD2a03";
