338939005d7cba05d96a9655bebbcf48239f3a66
Vlad Manoilo <48211890+vladosxxx@users.noreply.github.com>
2022-05-15 16:21:55 +0300

    fixed responsive mainpic

--- pages/MainPage.jsx
+++ pages/MainPage.jsx
@@ -15,12 +15,10 @@ const MainPage = () => {
   return (
     <main className="flex flex-col my-3 max-w-7xl px-4 sm:my-12 sm:px-6 md:my-16 lg:my-20 lg:px-8 xl:my-28">
       <div className="flex justify-center">
-        <div className="sm:text-center lg:text-left">
+        <div className="text-left">
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span className="block xl:inline dark:text-gray-300">
-              Send Crypto
-            </span>{" "}
-            <br className="hidden lg:block" />
+            <span className="block dark:text-gray-300">Send Crypto</span>{" "}
+            {/*<br className="hidden lg:block" />*/}
             <span className="block text-indigo-600 xl:inline">
               across the world
             </span>
@@ -28,7 +26,7 @@ const MainPage = () => {
           <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
             Buy, sell and exchange crypto around the world with small taxes.
           </p>
-          <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+          <div className="mt-5 sm:mt-8 sm:flex justify-start">
             {currentAccount ? (
               <MenuButton
                 name="Wallet is connected"
@@ -57,9 +55,9 @@ const MainPage = () => {
           {/*  clickFunc={() => getFunc()}*/}
           {/*/>*/}
         </div>
-        <div className="hidden lg:block flex self-center w-1/2">
+        <div className="block flex self-center w-1/2">
           <Image
-            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+            className="h-56 pl-1 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
             src={CryptoImg}
             alt=""
           />
