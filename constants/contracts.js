7bcfb454f6a828e86b0957c7c4e6c115c85d00b9
Vlad Manoilo <48211890+vladosxxx@users.noreply.github.com>
2022-05-15 14:45:05 +0300

    added json transaction to client

--- constants/contracts.js
+++ constants/contracts.js
@@ -1,4 +1,5 @@
 import abi from "../smart_contract/artifacts/contracts/Transaction.sol/Transaction.json";
+import abiLocal from "./Transaction.json";
 
-export const contractABI = abi.abi;
+export const contractABI = abi.abi || abiLocal.abi;
 export const contractAddress = "0xb96f1a785220D2A6ce392ee67dB6B7704FAD2a03";
