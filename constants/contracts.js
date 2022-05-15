ae506b7d87c887e012dd49787ea12b1db206eed8
Vlad Manoilo <48211890+vladosxxx@users.noreply.github.com>
2022-05-15 14:48:28 +0300

    fixed abi path

--- constants/contracts.js
+++ constants/contracts.js
@@ -1,5 +1,5 @@
-import abi from "../smart_contract/artifacts/contracts/Transaction.sol/Transaction.json";
-import abiLocal from "./Transaction.json";
+// import abi from "../smart_contract/artifacts/contracts/Transaction.sol/Transaction.json";
+import abiClient from "./Transaction.json";
 
-export const contractABI = abi.abi || abiLocal.abi;
+export const contractABI = abiClient.abi;
 export const contractAddress = "0xb96f1a785220D2A6ce392ee67dB6B7704FAD2a03";
