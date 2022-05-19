8da6746990d3b9713f1e403fb51ed41003226b23
Vlad Manoilo <48211890+vladosxxx@users.noreply.github.com>
2022-05-19 23:58:17 +0300

    Update CryptoCurency.jsx

--- pages/CryptoCurency.jsx
+++ pages/CryptoCurency.jsx
@@ -33,7 +33,7 @@ const CryptoCurency = () => {
             </span>
           </h1>
           <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-            check stocks of your favorite crypto
+            check charts of your favorite crypto
           </p>
         </div>
         <div className="block flex justify-start self-center w-1/2">
