fdb41fe7614e416b078e25772933cea2d5fc4fc9
Vlad Manoilo <48211890+vladosxxx@users.noreply.github.com>
2022-05-19 23:57:48 +0300

    pic charts

--- pages/CryptoCurency.jsx
+++ pages/CryptoCurency.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import Image from "next/image";
 
+import Image from "next/image";
+import CryptoImg from "../assets/images/crypto-curency.png";
 import log from "tailwindcss/lib/util/log";
 import Spinner from "../components/Spinner";
 
@@ -20,113 +21,119 @@ const CryptoCurency = () => {
       });
   }, []);
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              Name
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Price
-            </th>
-            <th scope="col" className="px-6 py-3">
-              24h %
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Market Cap
-            </th>
-            <th scope="col" className="px-6 py-3">
-              <span className="sr-only">Edit</span>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {isLoading ? (
-            <div>
-              <Spinner />
-            </div>
-          ) : (
-            data.map((crypto) => {
-              let rounded = parseFloat(crypto.priceUsd)
-                .toFixed(2)
-                .toString()
-                .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, " ");
-              let img = crypto.symbol.toLocaleLowerCase();
-              let percent24 = parseFloat(crypto.changePercent24Hr).toFixed(2);
-              let marketCup = parseFloat(crypto.marketCapUsd)
-                .toFixed(2)
-                .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, " ");
-              return (
-                <>
-                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-slate-100 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-                    >
-                      <img
-                        src={`https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/128/color/${img}.png`}
-                        alt={crypto.symbol}
-                        className="block w-8"
-                      />
-                      <span>{crypto.name}</span>
-                    </th>
-                    <td className="px-6 py-4">$ {rounded}</td>
-                    <td className="px-6 py-4">{percent24}%</td>
-                    <td className="px-6 py-4">$ {marketCup}</td>
-                    <td className="px-6 py-4 text-right">
-                      <a
-                        href="#"
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                      >
-                        More...
-                      </a>
-                    </td>
-                  </tr>
-                </>
-              );
-            })
-          )}
-        </tbody>
-      </table>
-    </div>
+    <main className="flex flex-col my-3 max-w-7xl px-4 sm:my-12 sm:px-6 md:my-16 lg:my-20 lg:px-8 xl:my-28">
+      <div className="flex justify-center mb-12">
+        <div className="text-left">
+          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+            <span className="block dark:text-gray-300">
+              Cryptocurrency Prices
+            </span>{" "}
+            <span className="block text-indigo-600 xl:inline">
+              stay up to date
+            </span>
+          </h1>
+          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+            check stocks of your favorite crypto
+          </p>
+        </div>
+        <div className="block flex justify-start self-center w-1/2">
+          <img
+            src={CryptoImg.src}
+            className="h-56 pl-1 w-full object-cover sm:h-72 md:h-96 lg:w-96 lg:h-full"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Price
+              </th>
+              <th scope="col" className="px-6 py-3">
+                24h %
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Market Cap
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Volume (24h)
+              </th>
+              <th scope="col" className="px-6 py-3">
+                VWAP (24h)
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <span className="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {isLoading ? (
+              <div>
+                <Spinner />
+              </div>
+            ) : (
+              data.map((crypto) => {
+                let rounded = parseFloat(crypto.priceUsd)
+                  .toFixed(2)
+                  .toString()
+                  .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, " ");
+                let img = crypto.symbol.toLocaleLowerCase();
+                let percent24 = parseFloat(crypto.changePercent24Hr).toFixed(2);
+                let marketCup = parseFloat(crypto.marketCapUsd)
+                  .toFixed(2)
+                  .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, " ");
+                let volume = parseFloat(crypto.volumeUsd24Hr)
+                  .toFixed(2)
+                  .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, " ");
+                let vwap = parseFloat(crypto.vwap24Hr)
+                  .toFixed(2)
+                  .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, " ");
 
-    // <div className="w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-    //   <a
-    //     href="#"
-    //     aria-current="true"
-    //     className="block flex flex-row justify-between w-full px-4 py-2 text-white bg-blue-700 border-b border-gray-200 rounded-t-lg cursor-pointer dark:bg-gray-800 dark:border-gray-600"
-    //   >
-    //     Crypto
-    //   </a>
-    //   {isLoading ? (
-    //     <div>
-    //       <Spinner />
-    //     </div>
-    //   ) : (
-    //     data.map((crypto) => (
-    //       <a
-    //         key={crypto.id}
-    //         href="#"
-    //         className="block flex flex-row justify-between w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
-    //       >
-    //         <img src={crypto.image} alt={crypto.name} className="block w-8" />
-    //         <span className="block">{crypto.name}</span>
-    //         <span className="block">{crypto.current_price} $</span>
-    //       </a>
-    //     ))
-    //   )}
-    // </div>
+                return (
+                  <>
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-slate-100 hover:bg-gray-50 dark:hover:bg-gray-600">
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+                      >
+                        <div className="flex flex-row">
+                          <img
+                            src={`https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/128/color/${img}.png`}
+                            alt={crypto.symbol}
+                            className="flex w-8"
+                          />
+                          <span className="ml-2 flex items-center">
+                            {crypto.name}
+                          </span>
+                        </div>
+                      </th>
+                      <td className="px-6 py-4">$ {rounded}</td>
+                      <td className="px-6 py-4">{percent24}%</td>
+                      <td className="px-6 py-4">$ {marketCup}</td>
+                      <td className="px-6 py-4">$ {volume}</td>
+                      <td className="px-6 py-4">$ {vwap}</td>
+                      <td className="px-6 py-4 text-right">
+                        <a
+                          href="#"
+                          className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                        >
+                          More...
+                        </a>
+                      </td>
+                    </tr>
+                  </>
+                );
+              })
+            )}
+          </tbody>
+        </table>
+      </div>
+    </main>
   );
 };
-// export const getServerSideProps = async () => {
-//   const res = await fetch("http://localhost:3000/api/markets");
-//   const data = await res.json();
-//
-//   return {
-//     props: {
-//       markets: data,
-//     },
-//   };
-// };
 export default CryptoCurency;
